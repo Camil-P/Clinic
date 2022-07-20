169e76192b25e988148de1b8f0b60c3e89bf261c
Camil Plojovic <camil.plojovic@levi9.com>
2022-07-20 15:21:32 +0200

    Fixed footer and header

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementsByClassName('header-toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('header-navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
-
