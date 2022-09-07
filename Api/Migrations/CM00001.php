f1ec2e9adfe781887e8c2d7ac492da85ad2031d8
Camil Plojovic <camil.plojovic@levi9.com>
2022-09-07 17:28:19 +0200

    configDB: created db files. CM00001 and CM00002 migrations

---
+++ Api/Migrations/CM00001.php
@@ -0,0 +1,15 @@
+<?php
+
+$descriptionCM00001 = "CM00001: Create appointment table";
+
+$CM00001 = "CREATE TABLE `appointment` (
+    `Id` int(11) NOT NULL AUTO_INCREMENT,
+    `ServiceName` varchar(25) NOT NULL,
+    `CompletionStatus` tinyint(1) NOT NULL DEFAULT '0',
+    `Date` date NOT NULL,
+    `StartingHour` int(11) NOT NULL,
+    `StartingMin` int(11) NOT NULL,
+    `PatientId` int(11) NOT NULL,
+    `DoctorId` int(11) NOT NULL,
+    PRIMARY KEY (Id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
