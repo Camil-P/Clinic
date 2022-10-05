01c98f50ded4e010827539748abc23186bfa5ed2
ddzelal <dzelal_d@live.com>
2022-10-05 22:37:04 +0200

    finished redirect from peges

--- Client/pages/patient/patient.js
+++ Client/pages/patient/patient.js
@@ -6,7 +6,7 @@ console.log("tu sam rodjace");
 logoutBtn?.addEventListener("click", (el) => {
   el.preventDefault();
 
-deleteCookie('accessToken')
-window.location.href = "http://127.0.0.1:5500/Client/index.html";
-
+  deleteCookie("accessToken");
+  deleteCookie("role");
+  window.location.href = "http://127.0.0.1:5500/Client/index.html";
 });
