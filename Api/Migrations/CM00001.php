56fcc96f31a905bd1de5f776bdef7f3fcbf74bd5
Camil Plojovic <camil.plojovic@levi9.com>
2022-10-06 21:23:35 +0200

    Added auth to appointment functionality

--- Api/Migrations/CM00001.php
+++ Api/Migrations/CM00001.php
@@ -5,12 +5,9 @@ $descriptionCM00001 = "CM00001: Create appointment table";
 $CM00001 = "CREATE TABLE `clinic`.`appointment` (
     `Id` int(11) NOT NULL AUTO_INCREMENT,
     `ServiceName` varchar(25) NOT NULL,
-    -- `CompletionStatus` tinyint(1) NOT NULL DEFAULT '0',
     `Date` date NOT NULL,
     `StartingHour` int(11) NOT NULL,
-    `StartingMin` int(11) NOT NULL,
     `PatientId` int(11) NOT NULL,
     `DoctorId` int(11) NOT NULL,
-    `Role` enum('Admin','Doctor','Patient') NOT NULL DEFAULT 'Patient',
     PRIMARY KEY (Id)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
