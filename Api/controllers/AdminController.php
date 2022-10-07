bbc242206194d4dadb41e25d011e2634d2a7257f
Camil Plojovic <camil.plojovic@levi9.com>
2022-10-07 15:51:37 +0200

    Added new htaccess config

--- Api/controllers/AdminController.php
+++ Api/controllers/AdminController.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Max-Age: 86400');        
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: *');
 
 include_once('../config/Database.php');
