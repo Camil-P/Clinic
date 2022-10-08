2940d486d5cad390b0905f142d0eef7848f5f74e
DzoniBlejz <alm_nikola@hotmail.com>
2022-10-09 00:25:01 +0200

    created dynamic table with patients for doctors panel

--- Client/pages/Doctor/doctor.js
+++ Client/pages/Doctor/doctor.js
@@ -25,13 +25,58 @@ const onSelectChange = () => {
 
 const showSelectedContent = (activeContent) => {
 	const container = document.getElementById("container-content");
-	console.log(container.children, "childnodes");
 	for (content of container.children) {
-		console.log("content.id", content);
 		if (content.id === activeContent) {
-			content.style.display = "block";
+			content.style.display = "flex";
 		} else {
 			content.style.display = "none";
 		}
 	}
 };
+const mockedData = [
+	{
+		name: "Nikola",
+		surname: "Glisovic",
+		email: "dzoniblejz2@gmail.com",
+		phoneNumber: 0666406404,
+	},
+	{
+		name: "Nikola",
+		surname: "Glisovic",
+		email: "dzoniblejz2@gmail.com",
+		phoneNumber: 0666406404,
+	},
+	{
+		name: "Nikola",
+		surname: "Glisovic",
+		email: "dzoniblejz2@gmail.com",
+		phoneNumber: 0666406404,
+	},
+	{
+		name: "Nikola",
+		surname: "Glisovic",
+		email: "dzoniblejz2@gmail.com",
+		phoneNumber: 0666406404,
+	},
+];
+
+const table = document.getElementById("table-patients");
+function createTableData() {
+	mockedData.forEach((e) => {
+		table.innerHTML += `<tbody><tr>
+		<td>${e.name}</td>
+		<td>${e.surname}</td>
+		<td>${e.email}</td>
+		<td>${e.phoneNumber}</td>
+	</tr>
+	</tbody>`;
+	});
+}
+createTableData();
+
+const btnLogout = document.getElementById("logout-doctor");
+btnLogout.addEventListener("click", () => {
+	deleteCookie("accessToken");
+	deleteCookie("role");
+	window.location.reload();
+});
