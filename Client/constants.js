aed62ea85191e2a72b79596800ac4f7e69b838f5
ddzelal <dzelal_d@live.com>
2022-10-17 13:48:49 +0200

    fixed bugs

--- Client/constants.js
+++ Client/constants.js
@@ -1,11 +1,9 @@
-
 const API_URL='https://traumatology-clinic.000webhostapp.com/controllers/';
-const ADMIN_CONTROLER=API_URL+'AdminController.php';
-const PATIENT_CONTROLER=API_URL+'PatientController.php';
-const MIGRATION=API_URL+'MigrationsController.php';
-const SESSION_URL=API_URL+'SessionController.php';
-const USER_CONTROLER=API_URL+'UserController.php';
-const APPOINTMENT_URL=API_URL+'AppointmentController.php';
-const DOCTOR_CONTROLER=API_URL+'DoctorController.php';
-
-
+// const API_URL = "http://127.0.0.1:5500/localhost/";
+const ADMIN_CONTROLER = API_URL + "AdminController.php";
+const PATIENT_CONTROLER = API_URL + "PatientController.php";
+const MIGRATION = API_URL + "MigrationsController.php";
+const SESSION_URL = API_URL + "SessionController.php";
+const USER_CONTROLER = API_URL + "UserController.php";
+const APPOINTMENT_URL = API_URL + "AppointmentController.php";
+const DOCTOR_CONTROLER = API_URL + "DoctorController.php";
