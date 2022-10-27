59e807e534ee6d847e0f2e202fc261653af712aa
Camil Plojovic <camil.plojovic@levi9.com>
2022-10-27 20:08:28 +0200

    fixed local production

--- Client/utils.js
+++ Client/utils.js
@@ -1,34 +1,34 @@
 
-const getCookie = (cookieName) => {
-  localStorage.getItem(cookieName);
-
-};
-
-function setCookie(
-  key,
-  value,
-  // expireSeconds
-  ) {
-    localStorage.setItem(key, value);
-  }
-  
-  function deleteCookie(name) {
-    localStorage.removeItem(name);
-  // setCookie(name, "", null, null, null, 1);
-}
-
-function get_cookie(name) {
-  localStorage.getItem(name);
+function getCookie(cookieName) {
+  return localStorage.getItem(cookieName);
  
-}
-
-
-const formatDate = (dateObj) => {
-let month = dateObj.getUTCMonth() + 1; //months from 1-12
-month = month.toString().length > 1 ? month : "0"+month;
-let day = dateObj.getUTCDate();
-day = day.toString().length > 1 ? day : "0"+day;
-let year = dateObj.getUTCFullYear();
-
-return year + "-" + month + "-" + day;
-};
+ }
+ 
+ function setCookie(
+   key,
+   value
+   // expireSeconds
+   ) {
+     localStorage.setItem(key, value);
+   }
+   
+   function deleteCookie(name) {
+     localStorage.removeItem(name);
+   // setCookie(name, "", null, null, null, 1);
+ }
+ 
+ function get_cookie(name) {
+   localStorage.getItem(name);
+  
+ }
+ 
+ 
+ const formatDate = (dateObj) => {
+ let month = dateObj.getUTCMonth() + 1; //months from 1-12
+ month = month.toString().length > 1 ? month : "0"+month;
+ let day = dateObj.getUTCDate();
+ day = day.toString().length > 1 ? day : "0"+day;
+ let year = dateObj.getUTCFullYear();
+ 
+ return year + "-" + month + "-" + day;
+ };
