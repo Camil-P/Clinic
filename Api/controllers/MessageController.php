aa56e58dcdbb11f15ffa6f31072b1fd515b29bf5
Camil Plojovic <camil.plojovic@levi9.com>
2022-10-30 18:46:32 +0100

    fixed message controller

--- Api/controllers/MessageController.php
+++ Api/controllers/MessageController.php
@@ -26,115 +26,97 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 $authorizedUser = authorize($writeDB);
 
-if (empty($_GET)) {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if (empty($_GET) && $_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+    if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+        $response = new Response(false, 400);
+        $response->addMessage("Content type header is not set to JSON");
+        $response->send();
+        exit();
+    }
+
+    try {
+
+        $rawPOSTData = file_get_contents('php://input');
+        $jsonData = json_decode($rawPOSTData);
+        if (!$jsonData) {
             $response = new Response(false, 400);
-            $response->addMessage("Content type header is not set to JSON");
+            $response->addMessage("Request body is not valid");
             $response->send();
             exit();
         }
 
-        try {
+        if ($jsonData->content === "" || !isset($jsonData->content) || empty($jsonData->content)) {
+            $response = new Response(false, 400);
+            $response->addMessage("Content of the message is not valid.");
+            $response->send();
+            exit();
+        }
 
-            $rawPOSTData = file_get_contents('php://input');
-            $jsonData = json_decode($rawPOSTData);
-            if (!$jsonData) {
-                $response = new Response(false, 400);
-                $response->addMessage("Request body is not valid");
-                $response->send();
-                exit();
-            }
+        $jsonData = (array)$jsonData;
+        $jsonData['sender'] = $authorizedUser['id'];
+        $jsonData = (object)$jsonData;
 
-            if ($jsonData->content === "" || !isset($jsonData->content) || empty($jsonData->content)){
-                $response = new Response(false, 400);
-                $response->addMessage("Content of the message is not valid.");
-                $response->send();
-                exit();
-            }
-
-            if ($authorizedUser['role'] === 'Patient') {
-
-                if (!isset($jsonData->doctorId) || !is_numeric($jsonData->doctorId) || is_string($jsonData->doctorId)){
-                    $response = new Response(false, 400);
-                    $response->addMessage("doctorId of the message is not valid.");
-                    $response->send();
-                    exit();
-                }
-
-                $jsonData = (array)$jsonData;
-                $jsonData['patientId'] = $authorizedUser['id'];
-                $jsonData = (object)$jsonData;
-            } else {
-                
-                if (!isset($jsonData->doctorId) || !is_numeric($jsonData->patientId) || is_string($jsonData->patientId)){
-                    $response = new Response(false, 400);
-                    $response->addMessage("patientId of the message is not valid.");
-                    $response->send();
-                    exit();
-                }
-
-                $jsonData = (array)$jsonData;
-                $jsonData['doctorId'] = $authorizedUser['id'];
-                $jsonData = (object)$jsonData;
-            }
-
-            $query = $writeDB->prepare("INSERT INTO message (
-                                            PatientId,
-                                            DoctorId,
+        $query = $writeDB->prepare("INSERT INTO message (
+                                            Sender,
+                                            Receiver,
                                             Content
                                         ) 
                                         values (
-                                            :patientId,
-                                            :doctorId,
+                                            :sender,
+                                            :receiver,
                                             :content);");
-            $query->bindParam(':patientId', $jsonData->patientId, PDO::PARAM_INT);
-            $query->bindParam(':doctorId', $jsonData->doctorId, PDO::PARAM_INT);
-            $query->bindParam(':content', $jsonData->content, PDO::PARAM_STR);
-            $query->execute();
+        $query->bindParam(':sender', $jsonData->sender, PDO::PARAM_INT);
+        $query->bindParam(':receiver', $jsonData->receiver, PDO::PARAM_INT);
+        $query->bindParam(':content', $jsonData->content, PDO::PARAM_STR);
+        $query->execute();
 
-            $rowCount = $query->rowCount();
-            if ($rowCount === 0) {
-                $response = new Response(false, 500);
-                $response->addMessage("Message was not created. Please try again");
-                $response->send();
-                exit();
-            }
-
-            $lastMessageCreated = $writeDB->lastInsertId();
-            
-            $jsonData = (array)$jsonData;
-            $jsonData['id'] = $lastMessageCreated;
-
-            $response = new Response(true, 201);
-            $response->addMessage("Successfully created Message");
-            $response->setData($jsonData);
-            $response->send();
-            exit();
-        } catch (PDOException $ex) {
+        $rowCount = $query->rowCount();
+        if ($rowCount === 0) {
             $response = new Response(false, 500);
-            $response->addMessage("There was a problem with creating a message in DB: " . $ex->getMessage());
+            $response->addMessage("Message was not created. Please try again");
             $response->send();
-
-            error_log("DB error: " . $ex->getMessage(), 0);
             exit();
         }
-    }
-}
-elseif(array_key_exists('id', $_GET) && ($_SERVER['REQUEST_METHOD'] === 'GET')){
 
-    if ($authorizedUser['role'] === 'Patient') {
-        $doctorId = $_GET['id'];
-        $query = $writeDB->prepare("SELECT * 
-                                    FROM message 
-                                    WHERE DoctorId = $doctorId;");
-    } else {
-        $patientId = $_GET['id'];
-        $query = $writeDB->prepare("SELECT * 
-                                    FROM message 
-                                    WHERE PatientId = $patientId;");
+        $lastMessageCreated = $writeDB->lastInsertId();
+
+        $jsonData = (array)$jsonData;
+        $jsonData['id'] = $lastMessageCreated;
+
+        $response = new Response(true, 201);
+        $response->addMessage("Successfully created Message");
+        $response->setData($jsonData);
+        $response->send();
+        exit();
+    } catch (PDOException $ex) {
+        $response = new Response(false, 500);
+        $response->addMessage("There was a problem with creating a message in DB: " . $ex->getMessage());
+        $response->send();
+
+        error_log("DB error: " . $ex->getMessage(), 0);
+        exit();
     }
+
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    // if ($authorizedUser['role'] === 'Patient') {
+    //     $doctorId = $_GET['id'];
+    //     $query = $writeDB->prepare("SELECT * 
+    //                                 FROM message 
+    //                                 WHERE DoctorId = $doctorId;");
+    // } else {
+    //     $patientId = $_GET['id'];
+    //     $query = $writeDB->prepare("SELECT * 
+    //                                 FROM message 
+    //                                 WHERE PatientId = $patientId;");
+    // }
+
+    $query = $writeDB->prepare("SELECT * 
+                                FROM message 
+                                WHERE 
+                                    Sender = {$authorizedUser['id']} OR
+                                    Receiver = {$authorizedUser['id']};");
 
     $query->execute();
 
@@ -146,23 +128,26 @@ elseif(array_key_exists('id', $_GET) && ($_SERVER['REQUEST_METHOD'] === 'GET')){
         exit();
     }
 
+    $responseData = array();
+    $responseData['senderId'] = $authorizedUser['id'];
+
     $messageArray = array();
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
         $message = array();
-        $message['Id'] = $row['Id'];
-        $message['Content'] = $row['Content'];
-        $message['PatientId'] = $row['PatientId'];
-        $message['DoctorId'] = $row['DoctorId'];
+        $message['id'] = $row['Id'];
+        $message['content'] = $row['Content'];
+        $message['sender'] = $row['Sender'];
+        $message['receiver'] = $row['Receiver'];
         $messageArray[] = $message;
     }
+    $responseData['messages'] = $messageArray;
 
     $response = new Response(true, 200);
     $response->addMessage("Successfully fetched messages");
-    $response->setData($messageArray);
+    $response->setData($responseData);
     $response->send();
     exit();
-}
- else {
+} else {
     $response = new Response(false, 404);
     $response->addMessage("Request unknown.");
     $response->send();
--- Client/pages/patient/patient.js
+++ Client/pages/patient/patient.js
@@ -8,19 +8,18 @@ let appointments = [];
 
 const fetchDoctors = () => {
   axios
-    .get(
-      PATIENT_CONTROLLER+"?fetch=doctors",
-      {
-        headers: {
-          Authorization: token,
-        },
-      }
-    )
+    .get(PATIENT_CONTROLLER + "?fetch=doctors", {
+      headers: {
+        Authorization: token,
+      },
+    })
     .then((res) => {
       const doctorsList = res.data.data;
       //sort by your selected doctor
-      const falseFirst = doctorsList.sort((a, b) => Number(b.assigned) - Number(a.assigned));
-      createDoctorsList(falseFirst)
+      const falseFirst = doctorsList.sort(
+        (a, b) => Number(b.assigned) - Number(a.assigned)
+      );
+      createDoctorsList(falseFirst);
     })
     .catch((err) => {
       alert(err);
@@ -31,7 +30,7 @@ fetchDoctors();
 
 function createDoctorsList(listDoctors) {
   const doctorContainer = document.getElementById("doctors-container");
-  
+
   listDoctors.forEach((doctor) => {
     doctorContainer.innerHTML += `<div class="doctors-card">
     <div class="doctors-image">
@@ -44,7 +43,7 @@ function createDoctorsList(listDoctors) {
     <div class="doctors-info">
       <div class="doctors-info-container">
         <h2>Doctor Name: ${doctor.name}</h2>
-        <h2>Surname:${doctor.surname }</h2>
+        <h2>Surname:${doctor.surname}</h2>
         <h2>Email: ${doctor.email} </h2>
         <h2>Gender:${doctor.gender}</h2>
         <h2>Phone number:${doctor.phoneNumber}</h2>
@@ -53,9 +52,13 @@ function createDoctorsList(listDoctors) {
     </div>
     <div class="doctors-select">
       <div class="button-container">
-        ${doctor.assigned ?
-          `<button disabled class="btn-doctor-false">Your selected doctor</button>` :
-          `<button onclick="requestChange(${doctor.id})" id="${"d" + doctor.id}" class="btn-doctor">Request to change your doctor</button>`}
+        ${
+          doctor.assigned
+            ? `<button disabled class="btn-doctor-false">Your selected doctor</button>`
+            : `<button onclick="requestChange(${doctor.id})" id="${
+                "d" + doctor.id
+              }" class="btn-doctor">Request to change your doctor</button>`
+        }
         
         <button class="btn-doctor">Send message</button>
       </div>
@@ -77,7 +80,7 @@ const fetchAppointments = () => {
       displayUpcomingAppointments(appointments);
     })
     .catch(({ response }) => {
-      console.log(response.data,"fetchAppointments");
+      console.log(response.data, "fetchAppointments");
     });
 };
 
@@ -85,23 +88,21 @@ fetchAppointments();
 
 const requestChange = (id) => {
   axios
-  .post(PATIENT_CONTROLLER, 
-  JSON.stringify({ requestedDoctorsId: id }),
-  {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: token,
-    }
-  })
-  .then(({ data }) => {
-    console.log(data);
-    alert("Successfully created doctor change request!");
-  })
-  .catch(({ response }) => {
-    console.log(response);
-    alert(response.data.messages[0]);
-  });
-}
+    .post(PATIENT_CONTROLLER, JSON.stringify({ requestedDoctorsId: id }), {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    })
+    .then(({ data }) => {
+      console.log(data);
+      alert("Successfully created doctor change request!");
+    })
+    .catch(({ response }) => {
+      console.log(response);
+      alert(response.data.messages[0]);
+    });
+};
 
 const logoutBtn = document.querySelector("#logout-patient");
 
@@ -119,28 +120,28 @@ const btnClose = document.getElementById("close-modal");
 
 btn_profile.addEventListener("click", (el) => {
   axios
-  .get(
-    PATIENT_CONTROLLER+"?fetch=profile",
-    {
+    .get(PATIENT_CONTROLLER + "?fetch=profile", {
       headers: {
         Authorization: token,
       },
-    }
-  )
-  .then(({ data }) => {
-    modalProfile.style.display = "block";
-    document.getElementById('profileName').innerHTML = data.data.name;
-    document.getElementById('profileSurname').innerHTML = data.data.surname;
-    document.getElementById('profileEmail').innerHTML = data.data.email;
-    document.getElementById('profileBirthDate').innerHTML = data.data.birthDate;
-    document.getElementById('profileBirthPlace').innerHTML = data.data.birthPlace;
-    document.getElementById('profilePhoneNumber').innerHTML = data.data.phoneNumber;
-    document.getElementById('profileGender').innerHTML = data.data.gender;
-  })
-  .catch((err) => {
-    alert(err.response.data.messages[0]);
-    console.log(err);
-  });
+    })
+    .then(({ data }) => {
+      modalProfile.style.display = "block";
+      document.getElementById("profileName").innerHTML = data.data.name;
+      document.getElementById("profileSurname").innerHTML = data.data.surname;
+      document.getElementById("profileEmail").innerHTML = data.data.email;
+      document.getElementById("profileBirthDate").innerHTML =
+        data.data.birthDate;
+      document.getElementById("profileBirthPlace").innerHTML =
+        data.data.birthPlace;
+      document.getElementById("profilePhoneNumber").innerHTML =
+        data.data.phoneNumber;
+      document.getElementById("profileGender").innerHTML = data.data.gender;
+    })
+    .catch((err) => {
+      alert(err.response.data.messages[0]);
+      console.log(err);
+    });
 });
 
 btnClose.addEventListener("click", () => {
@@ -159,23 +160,19 @@ form.addEventListener(
       reqData[key] = key === "startingHour" ? parseInt(value) : value;
     }
 
-  axios
-      .post(
-        APPOINTMENT_URL,
-        JSON.stringify(reqData),
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: token,
-          },
-        }
-      )
+    axios
+      .post(APPOINTMENT_URL, JSON.stringify(reqData), {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      })
       .then((res) => {
         alert("Appointment created successfully.");
         window.location.reload();
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
         alert(err);
       });
   },
@@ -183,7 +180,7 @@ form.addEventListener(
 );
 
 const handleDateChange = (dateEl) => {
-  const dateVal = dateEl.value
+  const dateVal = dateEl.value;
   // console.log(dateEl.value);
   displayStartingHours(dateVal);
 };
@@ -196,79 +193,125 @@ const displayStartingHours = (date) => {
     .map((a) => a.startingHour);
 
   for (let i = 8; i < 16; i++) {
-    const optionEl = document.createElement('option');
+    const optionEl = document.createElement("option");
     optionEl.value = i;
-    if (appointmentStartingHours.includes(i)){
+    if (appointmentStartingHours.includes(i)) {
       optionEl.innerHTML = i + " Already appointed";
       optionEl.disabled = true;
-    }
-    else{
+    } else {
       optionEl.innerHTML = i;
     }
-      startingHourFormField.appendChild(optionEl);
+    startingHourFormField.appendChild(optionEl);
   }
 };
 
 const displayUpcomingAppointments = (appointments) => {
-  const upcomingAppointmentsContainer = document.getElementById("upcomingAppointments");
-  appointments.filter(a => new Date(a.date).getTime() >= new Date().getTime()).forEach(a => {
-    upcomingAppointmentsContainer.innerHTML += `
+  const upcomingAppointmentsContainer = document.getElementById(
+    "upcomingAppointments"
+  );
+  appointments
+    .filter((a) => new Date(a.date).getTime() >= new Date().getTime())
+    .forEach((a) => {
+      upcomingAppointmentsContainer.innerHTML += `
     <div>
       <h1>${a.serviceName}</h1>
       <h1>${a.date} ${a.startingHour}h</h1>
       <button onclick="cancelAppointment(${a.id})">cancel <br> appointment</button>
     </div>`;
-  });
-}
+    });
+};
 
 const cancelAppointment = (id) => {
-  axios.delete(
-    APPOINTMENT_URL+"?appointmentId="+id,
-    {
+  axios
+    .delete(APPOINTMENT_URL + "?appointmentId=" + id, {
       headers: {
         Authorization: token,
       },
-    }
-  )
-  .then((res) => {
-    alert("Appointment deleted successfully.");
-    window.location.reload();
-  })
-  .catch((err) => {
-    console.log(err)
-    alert(err);
-  });
-}
+    })
+    .then((res) => {
+      alert("Appointment deleted successfully.");
+      window.location.reload();
+    })
+    .catch((err) => {
+      console.log(err);
+      alert(err);
+    });
+};
 
-const data = [
-  {
-      "Id": 1,
-      "Content": "Sta radis?",
-      "PatientId": 2,
-      "DoctorId": 3
-  },
-  {
-      "Id": 3,
-      "Content": "Kad si stigo?",
-      "PatientId": 2,
-      "DoctorId": 3
-  },
-  {
-      "Id": 4,
-      "Content": "Juce...",
-      "PatientId": 2,
-      "DoctorId": 3
-  },
-  {
-      "Id": 5,
-      "Content": "A nisi mogo prekjuce.",
-      "PatientId": 2,
-      "DoctorId": 3
-  },
-  {
-      "Id": 6,
-      "Content": "Jelde?",
-      "PatientId": 2,
-      "DoctorId": 3
-  }
-]
+const messageData = {
+  senderId: 3,
+  messages: [
+    {
+      id: 1,
+      content: "Sta radis?",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 3,
+      content: "Kad si stigo?",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 4,
+      content: "Juce...",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 5,
+      content: "A nisi mogo prekjuce.",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 6,
+      content: "Jelde?",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 7,
+      content: "nakon update-a?",
+      sender: 3,
+      receiver: 2,
+    },
+    {
+      id: 8,
+      content: "sad sad as dsa da",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 9,
+      content: "ti dobljo",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 10,
+      content: "psssss tuda",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 11,
+      content: "?",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 12,
+      content: "asds dsa",
+      sender: 2,
+      receiver: 3,
+    },
+    {
+      id: 13,
+      content: "dfsfsdfdsfd",
+      sender: 2,
+      receiver: 3,
+    },
+  ],
+};
